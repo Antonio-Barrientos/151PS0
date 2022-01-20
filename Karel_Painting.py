8baf2af5f39055f0420304dbc0648ec1e64a35bc
Antonio-Barrientos <98049405+Antonio-Barrientos@users.noreply.github.com>
2022-01-19 19:08:50 -0800

    Update_Karel_Painting_ANTONIO_BARRIENTOS.py

--- Karel_Painting.py
+++ Karel_Painting.py
@@ -1,9 +1,9 @@
 #====================================================
 # Filename: Karel_Painting.py
 # 
-# Your name:
+# Your name:Antonio Barrientos
 # Who did you work with (if anyone)?:
-# Estimate for time spent (in hrs)?:
+# Estimate for time spent (in hrs)?: 1 hr?
 #====================================================
 
 # I've just laid out a basic starting function below, but remember that you
@@ -16,3 +16,44 @@ import karel
 def main():
     """ Function to cause Karel to paint 3 sides of its house and then go indoors. """
     # You can add your code below
+    turn_around()
+    paint_one_side()
+    turn_left()
+    move()
+    paint_one_side()
+    turn_left()
+    move()
+    paint_one_side()
+    go_inside()
+    
+    
+
+def paint_one_side():
+    while left_is_blocked():
+        if left_is_blocked():
+            put_beeper()
+        move()
+        
+
+
+def go_inside():
+    """ Tells Karel to head indoors"""
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+
+
+def turn_right():
+    """ Helping function to turn Karel 90 degrees to the right"""
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def turn_around():
+    """ Helping function to turn Karel 180 degrees"""
+    turn_left()
+    turn_left()
+

f807e55c1358be37046c50dcf1ca02710e2b1a11
Antonio-Barrientos <98049405+Antonio-Barrientos@users.noreply.github.com>
2022-01-23 17:05:11 -0800

    Update_Karel_Newspaper_ANTONIO_BARRIENTOS.py

--- Karel_Newspaper.py
+++ Karel_Newspaper.py
@@ -1,9 +1,9 @@
 #====================================================
 # Filename: Karel_Newspaper.py
 # 
-# Your name:
-# Who did you work with (if anyone)?:
-# Estimate for time spent (in hrs)?:
+# Your name: Antonio Barrientos
+# Who did you work with (if anyone)?: N/A
+# Estimate for time spent (in hrs)?: 0.5
 #====================================================
 
 # I've just laid out a basic starting function below. 
@@ -19,18 +19,53 @@ def main():
     """ Function to cause Karel to retrieve the newspaper. """
     # You can add your sequence of commands below here!
     # Remember you can use the defined helper functions!
+    move_to_newspaper()
+    pick_up-paper()
+    return_to_start()
 
 
 def move_to_newspaper():
     """ Helping function to move Karel to the newspaper location. """
+    move()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    move()
 
 
 
 def pick_up_paper():
     """ Helping function to have Karel pick up the newspaper. """
     # Yes, this will probably be VERY simple and short
+    pick_beeper()
 
 
 
 def return_to_start():
     """ Helping function to move Karel back to its starting position. """
+    turn_around()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_around()
+
+
+
+def turn_right():
+
+    """ Helping function to turn Karel 90 degrees to the right"""
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+
+def turn_around():
+    """ Helping function to turn Karel 180 degrees"""
+    turn_left()
+    turn_left()
+
